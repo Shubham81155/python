31b765641369519e66ec09adb75789627cad2094
Shubham81155 <shubham81155@gmail.com>
2023-11-05 12:35:00 +0530

    commit

---
+++ strFn.py
@@ -0,0 +1,46 @@
+a="harry!!!!!!! harry"
+print(len(a))
+
+print(a.upper())
+
+# strings are immutable
+
+print(a.lower())
+
+print(a.rstrip("!"))
+
+print(a.replace("harry","John"))
+
+print(a.split(" "))
+
+blogheading="Introduction to py"
+print(blogheading.capitalize())
+
+str= "Welcome to the console"
+print(len(str))
+
+print(len(str.center(50)))
+
+print(a.count("harry"))
+
+str= "Welcome to the console"
+print(str.endswith("!!!"))
+
+str= "Welcome to the console"
+print(str.endswith("to",4,10))
+
+str="his name is shubham,he is an honest man"
+print(str.find("is"))
+
+print(str.isalpha())
+
+print(str.isprintable())
+
+str="       "
+print(str.isspace())
+
+str="India is Great"
+print(str.istitle())
+
+
+ 
